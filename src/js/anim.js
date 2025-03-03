8330608de1fdeecc6234acb0cc5729a59629954e
shomirulislam4 <shomirulislam4@gmail.com>
2025-03-04 00:04:30 +0600

    updatye

---
+++ src/js/anim.js
@@ -0,0 +1,53 @@
+/* Scale Animation */
+$(function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                $(entry.target).addClass('scale__anim');
+            } else {
+                $(entry.target).removeClass('scale__anim');
+            }
+        });
+    });
+
+    const hiddenElements = $('.scale__anim__hide');
+    hiddenElements.each(function () {
+        observer.observe(this);
+    });
+});
+
+
+/* Blur Animtion */
+$(function () {
+    let lastScrollTop = 0;
+
+    const observer = new IntersectionObserver((entries) => {
+        const currentScrollTop = $(window).scrollTop();
+
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                $(entry.target).addClass('blur__anim');
+            }
+        });
+
+        lastScrollTop = currentScrollTop;
+    }, {
+        threshold: 0.5
+    });
+
+    $('.blur__anim__hide').each(function () {
+        observer.observe(this);
+
+        if (isInViewport(this)) {
+            $(this).addClass('blur__anim');
+        }
+    });
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top < window.innerHeight &&
+            rect.bottom >= 0
+        );
+    }
+});
