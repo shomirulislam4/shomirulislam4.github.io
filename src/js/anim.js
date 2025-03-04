51145971a5420cb1fe5e8d88f9990b7f0ac2f874
shomirulislam4 <shomirulislam4@gmail.com>
2025-03-04 08:36:33 +0600

    update

--- src/js/anim.js
+++ src/js/anim.js
@@ -51,3 +51,58 @@ $(function () {
         );
     }
 });
+
+/* Slide In Animtaion */
+$(function () {
+    let lastScrollTop = 0;
+
+    const observer = new IntersectionObserver((entries) => {
+        const currentScrollTop = $(window).scrollTop();
+
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                $(entry.target).addClass('slidein__anim');
+            }
+        });
+
+        lastScrollTop = currentScrollTop;
+    }, {
+        threshold: 0.5
+    });
+
+    $('.slidein__anim__hide').each(function () {
+        observer.observe(this);
+
+        if (isInViewport(this)) {
+            $(this).addClass('slidein__anim');
+        }
+    });
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top < window.innerHeight &&
+            rect.bottom >= 0
+        );
+    }
+});
+
+/*
+$(function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio >= 0.5) {
+                $(entry.target).addClass('slidein__anim');
+            } else {
+                $(entry.target).removeClass('slidein__anim');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    $('.slidein__anim__hide').each(function () {
+        observer.observe(this);
+    });
+});
+*/
--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,44 @@ $(function(){
         $('.sb__header .wrapper').removeClass('show');
     })
 })
+
+/* Hero Section Parallax Effect */
+$(function() {
+    if ($(window).width() > 767) {
+      $('.sib__hero__section').each(function() {
+          $(this).data('initialPosition', { left: 0, top: 0 });
+      });
+
+      $('.sib__hero__section').on('mousemove', function(event) {
+          var moveX = (event.pageX - $(this).offset().left) / $(this).width() - 0.5;
+          var moveY = (event.pageY - $(this).offset().top) / $(this).height() - 0.5;
+
+          $(this).find('.parallax__effect1').css({
+              'transform': 'translate(' + moveX * -20 + 'px, ' + moveY * -20 + 'px)'
+          });
+
+          $(this).find('.parallax__effect2').css({
+            'transform': 'translate(' + moveX * -5 + 'px, ' + moveY * -5 + 'px)'
+          });
+          
+          $(this).find('.parallax__effect3').css({
+            'transform': 'translate(' + moveX * -9 + 'px, ' + moveY * -9 + 'px)'
+          });
+
+          $(this).find('.parallax__effect4').css({
+            'transform': 'translate(' + moveX * -5 + 'px, ' + moveY * -5 + 'px)'
+          });
+
+          $(this).find('.parallax__effect5').css({
+            'transform': 'translate(' + moveX * -7 + 'px, ' + moveY * -7 + 'px)'
+          });
+
+        }).on('mouseleave', function() {
+            // Retrieve initial position and reset the image
+            var initialPosition = $(this).data('initialPosition');
+            $(this).find('.parallax__effect').css({
+                'transform': 'translate(0, 0)'
+            });
+        });
+    }
+});
